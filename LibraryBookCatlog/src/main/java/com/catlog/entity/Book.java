c8e9f2a9ec648df5b9417fe3d8a0b6f96f3aa974
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-14 21:47:24 +0530

    modifydata commit

--- LibraryBookCatlog/src/main/java/com/catlog/entity/Book.java
+++ LibraryBookCatlog/src/main/java/com/catlog/entity/Book.java
@@ -1,69 +1,27 @@
 package com.catlog.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
+@Setter
+@Getter
+@ToString
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private int id;
     private String name;
     private String author;
     private String isbn;
     private int availableCopies;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public int getAvailableCopies() {
-        return availableCopies;
-    }
-
-    public void setAvailableCopies(int availableCopies) {
-        this.availableCopies = availableCopies;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", author='" + author + '\'' +
-                ", isbn='" + isbn + '\'' +
-                ", availableCopies=" + availableCopies +
-                '}';
-    }
 }
