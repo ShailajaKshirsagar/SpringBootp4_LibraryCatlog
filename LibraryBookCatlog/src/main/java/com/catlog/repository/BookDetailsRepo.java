e2f33b76509fad0366d6fc581872ccc6fc8d99b1
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-24 20:50:34 +0530

    password commit

---
+++ LibraryBookCatlog/src/main/java/com/catlog/repository/BookDetailsRepo.java
@@ -0,0 +1,8 @@
+package com.catlog.repository;
+
+import com.catlog.entity.BookDetailsMapping;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookDetailsRepo extends JpaRepository <BookDetailsMapping, Integer>
+{
+}
