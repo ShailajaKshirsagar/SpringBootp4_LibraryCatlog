65ef182546c9f7ec6e9ac1f35cf8bec14856e2df
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-09-08 23:38:36 +0530

    Book  repo added

--- LibraryBookCatlog/src/main/java/com/catlog/repository/BookRepository.java
+++ LibraryBookCatlog/src/main/java/com/catlog/repository/BookRepository.java
@@ -3,7 +3,9 @@ package com.catlog.repository;
 import com.catlog.entity.Book;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Integer>
 {
 }
