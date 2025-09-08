91585b2ce0052a60f9b358cb2a7838dcf513f269
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-09-08 23:39:21 +0530

    Book  Category  repo added

--- LibraryBookCatlog/src/main/java/com/catlog/repository/BookCategoryRepo.java
+++ LibraryBookCatlog/src/main/java/com/catlog/repository/BookCategoryRepo.java
@@ -2,7 +2,9 @@ package com.catlog.repository;
 
 import com.catlog.entity.BookCategoryMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookCategoryRepo extends JpaRepository<BookCategoryMapping,Integer>
 {
 }
