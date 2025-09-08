11cacdc962c7da53bcf0ef27586da046da60d840
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-09-08 23:38:21 +0530

    Book details repo added

--- LibraryBookCatlog/src/main/java/com/catlog/repository/BookDetailsRepo.java
+++ LibraryBookCatlog/src/main/java/com/catlog/repository/BookDetailsRepo.java
@@ -2,7 +2,9 @@ package com.catlog.repository;
 
 import com.catlog.entity.BookDetailsMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookDetailsRepo extends JpaRepository <BookDetailsMapping, Integer>
 {
 
