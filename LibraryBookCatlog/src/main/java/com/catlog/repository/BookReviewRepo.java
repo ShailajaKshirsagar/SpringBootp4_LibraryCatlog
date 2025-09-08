f7507ef06ebb17b090551d3e74d059dd06ba2023
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-09-08 23:38:50 +0530

    Book  Review repo added

--- LibraryBookCatlog/src/main/java/com/catlog/repository/BookReviewRepo.java
+++ LibraryBookCatlog/src/main/java/com/catlog/repository/BookReviewRepo.java
@@ -2,6 +2,8 @@ package com.catlog.repository;
 
 import com.catlog.entity.BookReviewMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookReviewRepo extends JpaRepository<BookReviewMapping, Integer> {
 }
